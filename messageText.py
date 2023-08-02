f340f186ad16d46ed420b8a7428f1345d548bb42
oamankwa <108134334+oamankwa@users.noreply.github.com>
2023-08-02 12:59:44 -0400

    Update messageText.py 08/02/2023

--- messageText.py
+++ messageText.py
@@ -16,9 +16,11 @@ class Createmessage:
      
 
     #Write an  email 
-    def write_email(self):
+    def write_email(self from_email, to_email):
         msg = EmailMessage()
         msg['Subject'] = self.SUBJECT
+        msg['from'] = from_email
+        msg['to'] = to_email
         msg.set_content(self.BODY)
         return msg
         
